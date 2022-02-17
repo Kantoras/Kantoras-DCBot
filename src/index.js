cf13c0e6aa322a443bab5776dcc6e76627851478
Kantoras <85099575+Kantoras@users.noreply.github.com>
2022-02-17 17:07:55 +0100

    First Push

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const { Client } = require ("discord.js")
+
+const client = new Client({intents: []})
+
+client.once("ready", () => {
+    console.log(`Ready! Logged in as ${client.user.tag}! I am on ${client.guilds.cache.size} guild(s)!`)
+    client.user.setActivity({name: "mit dem Code", type: "PLAYING"})
+})
+
+
+client.login(process.env.DISCORD_BOT_TOKEN)
